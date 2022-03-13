2baf2304322ba19c7b7860686c709055f48ef442
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-13 16:58:10 -0400

    connected MySQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,19 @@ app.get('/', (req, res) => {
         message: 'Hello World'
     });
 });
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'L10nh34rt86.',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
